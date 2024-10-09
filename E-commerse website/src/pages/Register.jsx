eb4cc13fc1ffccd677ae4b43b6446f191608933b
Akshaykumar1679 <ak1010doon@gmail.com>
2024-10-09 16:14:06 +0530

    update login or registration

--- E-commerse website/src/pages/Register.jsx
+++ E-commerse website/src/pages/Register.jsx
@@ -19,7 +19,7 @@ function Register() {
     <div className="flex flex-col gap-3 border-2 rounded border-slate-500 shadow-md shadow-red-500 py-3 px-5 min-w-[60%]">
       <div className="flex flex-col gap-3">
         <h1 className="text-4xl font-semibold ">Create a new account</h1>
-        <p className="text-2xl font-semibold ">already have a account <Link to="/auth/login2" className="text-red-400 hover:underline">LogIn</Link> </p>
+        <p className="text-2xl font-semibold ">already have a account <Link to="/auth/login" className="text-red-400 hover:underline">LogIn</Link> </p>
       </div>
       <Form
         formControls={registerFormcontrols}
